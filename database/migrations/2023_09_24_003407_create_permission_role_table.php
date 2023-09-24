16523805a5dde4f016b69c292493b2e05dd64125
Tojan Aklook <tojannafe@gmail.com>
2023-09-24 04:43:15 +0300

    gaza store project

--- database/migrations/2023_09_24_003407_create_permission_role_table.php
+++ database/migrations/2023_09_24_003407_create_permission_role_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('permission_role', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->enum('type', ['admin', 'employee', 'customer'])->default('customer');
+            $table->foreignId('permission_id')->constrained('permissions')->cascadeOnDelete();
+            $table->foreignId('role_id')->constrained('roles')->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('permition_role');
     }
 };
